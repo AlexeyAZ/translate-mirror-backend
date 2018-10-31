9affadb47f67cb7e53a9124e305ae1fc82750a60
Alexey <teos.nl@gmail.com>
2018-10-31 15:13:08 +0300

    small change

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
-userRouter.post('/create', userController.createUser);
 userRouter.get('/', userController.getAllUsers);
 userRouter.get('/:userId', userController.getUser);
+userRouter.post('/create', userController.createUser);
 
 module.exports = userRouter;
